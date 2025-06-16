e1c4130a6b321718b7a1233f6b1049cffc2ae057
Bertuğ Kalkan <kalkanbertug@gmail.com>
2025-06-17 01:46:02 +0300

    main

---
+++ app/index.mjs
@@ -0,0 +1,65 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' },
+    { id: 3, name: 'John Smith' },
+    { id: 4, name: 'Jane Smith' },
+]
+
+const products = [
+    { id: 1, name: 'Product 1' },
+    { id: 2, name: 'Product 2' },
+    { id: 3, name: 'Product 3' },
+    { id: 4, name: 'Product 4' },
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/users', (request, response) => {
+    const {
+        query: {filter, value}
+    } = request;
+
+    // Eğer filter ve value parametreleri yoksa tüm kullanıcıları döndür
+    if (!filter || !value) {
+        return response.status(200).json(users);
+    }
+
+    // Filtreleme işlemi
+    const filteredUsers = users.filter(user => {
+        if (user[filter] === undefined) {
+            return false;
+        }
+        return String(user[filter]).toLowerCase().includes(String(value).toLowerCase());
+    });
+
+    return response.status(200).send(filteredUsers);
+});
+
+app.post('/api/users', (request, response) => {
+    const {
+        body: {name}
+    } = request;
+    const newUser = {id: users.length + 1, name};
+    users.push(newUser);
+    return response.status(201).send(newUser);
+});
+
+app.get('/api/users/:id', (request, response) => {
+    const user = users.find(user => user.id === parseInt(request.params.id));
+    if (!user) {
+        response.status(404).send('User not found');
+    }
+    return response.status(200).send(user);
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
